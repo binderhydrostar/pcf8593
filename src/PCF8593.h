b64fcf326599782e5e34345b569fc3db33f28f1f
Marciel Dechert <85219503+MDechert@users.noreply.github.com>
2024-06-12 10:07:06 +0200

    Create PCF8593.h

---
+++ src/PCF8593.h
@@ -0,0 +1,36 @@
+#include "PCF8583.h"
+#include <Wire.h>
+
+namespace as {
+  class PCF8593 {
+    PCF8583 _pcf8593;
+    
+    public:
+      PCF8593() : _pcf8593(0xA2) {};
+
+      void init() {
+        _pcf8593.setRegister(LOCATION_CONTROL, 0x80);     
+        _pcf8593.setCount(0);       
+        _pcf8593.setRegister(LOCATION_CONTROL, MODE_EVENT_COUNTER);
+        if ((_pcf8593.getRegister(LOCATION_CONTROL)) && 0x20 == 0x20) {
+          Serial.println("PCF8593 init done, event counter mode set.");
+        } else {
+          Serial.println("PCF8593 init: setting event counter mode FAILED!");       
+        }      
+      }
+
+      uint16_t getCount() {
+        return _pcf8593.getCount();
+      }
+
+      void resetCounter() {
+        uint8_t controlReg = _pcf8593.getRegister(LOCATION_CONTROL);
+        controlReg |= 0x80;
+        //datasheet recommends stopping counter before setting value
+        _pcf8593.setRegister(LOCATION_CONTROL, controlReg);  
+        _pcf8593.setCount(0);
+        controlReg &= 0x7F;      
+        _pcf8593.setRegister(LOCATION_CONTROL, controlReg);  
+      }
+  };
+}
